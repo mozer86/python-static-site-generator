01a71735aac56b3cdf7d54951fcda078c7981785
mozer86 <mehmetozer.bim@gmail.com>
2022-04-30 19:16:14 +0300

    finitto

--- ssg.py
+++ ssg.py
@@ -2,12 +2,16 @@ import typer
 from ssg.site import Site
 import ssg.parsers
 
-def main(source = "content", dest = "dist"):
+def main(source="content", dest="dist"):
     config = {
         "source": source,
         "dest": dest,
-        "parsers": [ssg.parsers.ResourceParser(),ssg.parsers.MarkdownParser(),ssg.parsers.ReStructuredParser()],
-        }
+        "parsers": [
+            ssg.parsers.ResourceParser(),
+            ssg.parsers.MarkdownParser(),
+            ssg.parsers.ReStructuredTextParser(),
+        ],
+    }
     Site(**config).build()
 
 typer.run(main)
