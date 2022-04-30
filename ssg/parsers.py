8d9e3df9ddc9bdf86cc504f92bb8698d66750ce6
mozer86 <mehmetozer.bim@gmail.com>
2022-04-30 19:15:05 +0300

    finished

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -48,11 +48,13 @@ class MarkdownParser(Parser):
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
 
-class ReStructuredParser(Parser):
+class ReStructuredTextParser(Parser):
     extensions = [".rst"]
 
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
-        html = publish_parts(content.body, writer_name = "html5")
+        html = publish_parts(content.body, writer_name="html5")
         self.write(path, dest, html["html_body"])
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
+        sys.stdout.write(
+            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
+        )
