96fcfa9fb042e0d884e0230aacb3046100ad9cde
mozer86 <mehmetozer.bim@gmail.com>
2022-04-30 19:12:55 +0300

    finished

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -9,7 +9,7 @@ from ssg.content import Content
 
 class Parser:
 
-    extensions : List[str] = []
+    extensions: List[str] = []
 
     def valid_extension(self, extension):
         return extension in self.extensions
@@ -51,7 +51,7 @@ class ReStructuredParser:
     extensions = [".rst"]
 
     def parse(self, path, source, dest):
-        content = Content.Load(self.read(path))
+        content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name = "html5")
         self.write(path, dest, html["html_body"])
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
